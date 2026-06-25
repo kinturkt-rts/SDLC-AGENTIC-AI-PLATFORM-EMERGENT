5c4b4cd2c587d1af03ff6c1ec4b6c3f125f85ca1
Kintur Shah <kintur.shah@resolvetech.com>
2026-06-24 21:46:11 -0500

    Pushed new changes

--- target-apps/support-knowledge-hub/db/sql/001_enable_pgvector.sql
+++
@@ -1,2 +0,0 @@
--- 001_enable_pgvector.sql
-CREATE EXTENSION IF NOT EXISTS vector WITH SCHEMA public;
--- target-apps/support-knowledge-hub/db/sql/002_schema.sql
+++
@@ -1,4 +0,0 @@
--- 002_schema.sql
--- Create app schema
-CREATE SCHEMA IF NOT EXISTS support_knowledge_hub;
-SET search_path TO support_knowledge_hub, public;
